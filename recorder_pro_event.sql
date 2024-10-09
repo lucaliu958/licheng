82c0dfb6afb4dfaa98b4d8227b629b0262db7905
lucaliu958 <xuewang@atlasv.com>
2024-10-09 15:55:03 +0800

    Update recorder_pro_event.sql

--- recorder_pro_event.sql
+++ recorder_pro_event.sql
@@ -446,6 +446,7 @@ SELECT
   traffic_source_type,
   traffic_source_name,
   app_version
+  ,traffic_source_medium
 FROM (
   SELECT
     PARSE_DATE('%Y%m%d',event_date) event_date,
@@ -465,6 +466,8 @@ FROM (
     app_info.version app_version,
     event_params.key AS event_params_key,
     COALESCE(CAST(event_params.value.string_value AS string),CAST(event_params.value.int_value AS string),CAST(event_params.value.float_value AS string),CAST(event_params.value.double_value AS string)) AS event_params_value
+   ,case when traffic_source.medium='' or traffic_source.medium is null then 'undefined' else traffic_source.medium end as traffic_source_medium
+
   FROM
     `recorder-pro-50451.analytics_250268757.events_*`
   CROSS JOIN
@@ -498,6 +501,7 @@ GROUP BY
   event_name,
   event_params_key,
   event_params_value,
+  traffic_source_medium,
   app_version;
 
 
