46439ac67371e80b2e2f40c88364651311b38e7c
lucaliu958 <xuewang@atlasv.com>
2024-11-25 14:24:21 +0800

    Update fbaiavatar_roi_event_pro.sql
    增加参数

--- fbaiavatar_roi_event_pro.sql
+++ fbaiavatar_roi_event_pro.sql
@@ -31,6 +31,8 @@ insert `gzdw2024.fbgame_01_basic.dwd_all_game_user_event_di`
 		,device.operating_system as operating_system
 	,(SELECT COALESCE(cast(value.int_value as string),cast(value.string_value as string),cast(value.float_value as string),cast(value.double_value as string)) FROM UNNEST(event_params) WHERE key='placement') placement
 		,(SELECT value.int_value FROM UNNEST(event_params) WHERE key='timeuse') as timeuse
+	,(SELECT COALESCE(cast(value.int_value as string),cast(value.string_value as string),cast(value.float_value as string),cast(value.double_value as string)) FROM UNNEST(event_params) WHERE key='placement') placement
+		,(SELECT value.int_value FROM UNNEST(event_params) WHERE key='steps') as steps
 	FROM `recorder-pro-50451.analytics_250268757.events_*`
 	WHERE 1=1
 	 and  stream_id in('9692329810','9817620337')
