40168d709ce6d22824012b555c2ed00e1f7cb934
lucaliu958 <xuewang@atlasv.com>
2024-11-26 14:33:21 +0800

    Update fb_zp_event_pro.sql
    增加stpes和error_code

--- fb_zp_event_pro.sql
+++ fb_zp_event_pro.sql
@@ -35,6 +35,8 @@ insert `gzdw2024.fb_zp_game.dwd_user_event_di`
 		,operating_system
 		,placement
 		,timeuse
+	,steps
+	,error_code
 	FROM `gzdw2024.fbgame_01_basic.dwd_all_game_user_event_di`
 	WHERE event_date>=date_add(run_date,interval -history_day day)
 	and event_date<=date_add(run_date,interval -history_end_day day)
