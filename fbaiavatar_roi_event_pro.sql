1ab8685033fb9eb0531ca52fb4fb6b617029280c
lucaliu958 <xuewang@atlasv.com>
2024-12-18 10:13:29 +0800

    Update fbaiavatar_roi_event_pro.sql
    增加参数

--- fbaiavatar_roi_event_pro.sql
+++ fbaiavatar_roi_event_pro.sql
@@ -46,6 +46,8 @@ insert `gzdw2024.fbgame_01_basic.dwd_all_game_user_event_di`
 	,(SELECT COALESCE(cast(value.int_value as string),cast(value.string_value as string),cast(value.float_value as string),cast(value.double_value as string)) FROM UNNEST(event_params) WHERE key='coinsum') proptype
 	,(SELECT COALESCE(cast(value.int_value as string),cast(value.string_value as string),cast(value.float_value as string),cast(value.double_value as string)) FROM UNNEST(event_params) WHERE key='propsum') propsum
 	,(SELECT COALESCE(cast(value.int_value as string),cast(value.string_value as string),cast(value.float_value as string),cast(value.double_value as string)) FROM UNNEST(event_params) WHERE key='buytype') buytype
+	,(SELECT COALESCE(cast(value.int_value as string),cast(value.string_value as string),cast(value.float_value as string),cast(value.double_value as string)) FROM UNNEST(event_params) WHERE key='entrance') entrance
+		,(SELECT COALESCE(cast(value.int_value as string),cast(value.string_value as string),cast(value.float_value as string),cast(value.double_value as string)) FROM UNNEST(event_params) WHERE key='eventTime') eventTime
 	FROM `recorder-pro-50451.analytics_250268757.events_*`
 	WHERE 1=1
 	 and  stream_id in('9692329810','9817620337')
