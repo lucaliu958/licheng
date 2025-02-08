bd3b5f1cefedffe600f4be7d1bee665b86c2d817
lucaliu958 <xuewang@atlasv.com>
2025-02-08 11:54:30 +0800

    Update fb_common_game_roi_task.sql
    增加block成本数据

--- fb_common_game_roi_task.sql
+++ fb_common_game_roi_task.sql
@@ -25,7 +25,10 @@ begin
 			FROM `gzdw2024.cost_data.fb_game_egg_cost_data`
 			UNION ALL
 			SELECT string_field_0,string_field_1,string_field_3,string_field_6,'fb.bubble.shoot.pro'  as package_name
-			FROM `gzdw2024.cost_data.fb_game_bubblepro_cost_data`;
+			FROM `gzdw2024.cost_data.fb_game_bubblepro_cost_data`
+			UNION ALL
+			SELECT string_field_0,string_field_1,string_field_3,string_field_6,'fb.block.juggle'  as package_name
+			FROM `gzdw2024.cost_data.fb_game_block_cost_data`;
 
 
 			CREATE OR REPLACE VIEW `gzdw2024.cost_data.all_external_data_platform` AS
