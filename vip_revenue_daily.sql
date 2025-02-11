13cf33988e765fd38e4eee87390932790d0d679a
lucaliu958 <xuewang@atlasv.com>
2025-02-11 18:31:10 +0800

    Update vip_revenue_daily.sql
    修改gp收入计算方式

--- vip_revenue_daily.sql
+++ vip_revenue_daily.sql
@@ -1,36 +1,104 @@
 CREATE OR REPLACE PROCEDURE `gzdw2024.gz_dim.vip_revenue_daily`(run_date DATE, history_day INT64, end_day INT64)
 begin
 
+---------gp订阅收，广州所有产品
 
------------gp订阅收入,vidma
-delete gzdw2024.revenue.dws_app_country_vip_income
-where stats_date>=date_add(run_date,interval -history_day day)
-and stats_date<=date_add(run_date,interval -end_day day)
-and package_name in ( SELECT package_name FROM `gzdw2024.gz_dim.app_info` where vip_income_table='gzdw2024.googleplay.p_sales_vidma');
+delete gzdw2024.googleplay.dwd_gp_sales_event_di
+WHERE Order_Charged_Date >=date_add(run_date,interval -history_day day)
+and Order_Charged_Date<=date_add(run_date,interval -end_day day) 
+and Product_ID in (SELECT		package_name 
+		FROM `gzdw2024.gz_dim.app_info` 
+		where platform='android'
+		group by package_name
+		);
 
 
-	insert    gzdw2024.revenue.dws_app_country_vip_income 
+	insert    gzdw2024.googleplay.dwd_gp_sales_event_di 
+--drop table if exists `gzdw2024.googleplay.dwd_gp_sales_event_di`;
+--create table `gzdw2024.googleplay.dwd_gp_sales_event_di`
+--	PARTITION BY Order_Charged_Date as 
+SELECT
+	stats_mon
+	,Product_ID 
+	,Order_Number
+	,Order_Charged_Date
+	,Product_Title
+	,Product_Type
+	,Country_of_Buyer
+	,Currency_of_Sale
+	,Financial_Status
+	,Item_Price
+	,Charged_Amount
+	,Taxes_Collected	
+	,SKU_ID
+	,google_fee_rate
+	,round(rate,4) as rate
+	,safe_divide(Charged_Amount,rate) as Charged_Amount_usd
+	,safe_divide(Taxes_Collected,rate) as Taxes_Collected_usd
+	--,safe_divide(Item_Price,rate) as Item_Price_usd
+	,round(safe_divide(Charged_Amount - Taxes_Collected,rate),4) as Revenue_Without_VAT
+	,round(safe_divide(Charged_Amount - Taxes_Collected,rate)*google_fee_rate,4) as Google_Fee
+	,round(safe_divide(Charged_Amount - Taxes_Collected,rate) - safe_divide(Charged_Amount - Taxes_Collected,rate)*google_fee_rate,4) as Revenue
+FROM
+	(
 	SELECT
-		Order_Charged_Date as stats_date
-		,package_name
-		,upper(country_code) as country_code
-		,sum(case when Financial_Status='Charged' then  Item_Price/rate else 0 end) as charged_money
-		,sum(case when Financial_Status='Refund' then  Item_Price/rate else 0 end) as refund_money  
-		,sum(case when Financial_Status='Charged' then  Item_Price/rate else 0 end)- sum(case when Financial_Status='Refund' then  Item_Price/rate else 0 end) as vip_revenue
+		a.stats_mon
+		,Product_ID 
+		,Order_Number
+		,Order_Charged_Date
+		,Product_Title
+		,Product_Type
+		,Country_of_Buyer
+		,Currency_of_Sale
+		,Financial_Status
+		,Item_Price
+		,Charged_Amount
+		,Taxes_Collected	
+		,SKU_ID
+		,google_fee_rate
+		,case when b1.rate is not null then b1.rate else b.rate end as rate 
 	FROM
 		(
 		SELECT 
 		Product_ID 
-		,Charged_Amount
-		,ARRAY[Country_of_Buyer,'TOTAL'] as country_code
+		,Order_Number
+		,Order_Charged_Date
+		,Product_Title
+		,Product_Type
+		,Country_of_Buyer
 		,Currency_of_Sale
 		,Financial_Status
-		,Order_Charged_Date
 		,Item_Price
+		,Charged_Amount
+		,Taxes_Collected
 		,substring(cast(Order_Charged_Date as string),1,7) as stats_mon
-		FROM `gzdata.googleplay.p_sales_vidma` 
-		WHERE Order_Charged_Date >=date_add(run_date,interval -history_day day)
-		and Order_Charged_Date<=date_add(run_date,interval -end_day day)
+		,LOWER(SKU_ID) AS SKU_ID
+		 ,CASE WHEN lower(Product_Type) ='subscription' 
+		     THEN 0.15
+		   WHEN lower(Product_Type) ='inapp' 
+		     THEN 0.3
+		     ELSE 0.3
+		  END AS google_fee_rate
+		FROM
+			(
+			SELECT
+				*
+			FROM `gzdata.googleplay.p_sales_vidma` 
+			WHERE Order_Charged_Date >=date_add(run_date,interval -history_day day)
+			and Order_Charged_Date<=date_add(run_date,interval -end_day day)
+			union all 
+			SELECT
+				*
+			FROM `hzdw2024.googleplay.p_sales_downloader` 
+			WHERE Order_Charged_Date >=date_add(run_date,interval -history_day day)
+			and Order_Charged_Date<=date_add(run_date,interval -end_day day)
+			union all 
+			SELECT
+				*
+			FROM `hzdw2024.googleplay.p_sales_new_downloader`  
+			WHERE Order_Charged_Date >=date_add(run_date,interval -history_day day)
+			and Order_Charged_Date<=date_add(run_date,interval -end_day day)
+			)
 		)a 
 		left join 
 		(
@@ -42,6 +110,18 @@ and package_name in ( SELECT package_name FROM `gzdw2024.gz_dim.app_info` where 
 		)b 
 		on a.stats_mon=b.stats_mon
 		and a.Currency_of_Sale=b.currency
+			left join 
+		(
+		SELECT 
+		cast(stats_date as date) as stats_date
+		,currency
+		,rate 
+		FROM `gzdw2024.gz_dim.exchange_rate_days` 
+		where cast(stats_date as date)>=date_add(run_date,interval -history_day day)
+		and cast(stats_date as date)<=date_add(run_date,interval -end_day day)
+		)b1 
+		on a.Order_Charged_Date=b1.stats_date
+		and a.Currency_of_Sale=b1.currency
 		join 
 		(
 		SELECT 
@@ -51,116 +131,42 @@ and package_name in ( SELECT package_name FROM `gzdw2024.gz_dim.app_info` where 
 		group by package_name
 		)c 
 		on a.Product_ID=c.package_name
-		,UNNEST(country_code) as country_code
-		GROUP BY package_name,Order_Charged_Date,country_code;
+	)d; 
 
-
-
------------gp订阅收入,all3
+-----------gp订阅收入所有产品
 delete gzdw2024.revenue.dws_app_country_vip_income
-where stats_date>=date_add(run_date,interval -history_day day)
-and stats_date<=date_add(run_date,interval -end_day day)
-and package_name in ( SELECT package_name FROM `gzdw2024.gz_dim.app_info` where vip_income_table='hzdw2024.googleplay.p_sales_new_downloader');
+WHERE stats_date >=date_add(run_date,interval -history_day day)
+and stats_date<=date_add(run_date,interval -end_day day) 
+and package_name in (SELECT		package_name 
+		FROM `gzdw2024.gz_dim.app_info` 
+		where platform='android'
+		group by package_name
+		);
 
-
-
-	insert    gzdw2024.revenue.dws_app_country_vip_income 
+insert    gzdw2024.revenue.dws_app_country_vip_income 
 	SELECT
 		Order_Charged_Date as stats_date
 		,package_name
 		,upper(country_code) as country_code
-	,sum(case when Financial_Status='Charged' then  Item_Price/rate else 0 end) as charged_money
-		,sum(case when Financial_Status='Refund' then  Item_Price/rate else 0 end) as refund_money  
-		,sum(case when Financial_Status='Charged' then  Item_Price/rate else 0 end)- sum(case when Financial_Status='Refund' then  Item_Price/rate else 0 end) as vip_revenue
+		,sum(case when Revenue>=0 then Revenue else 0 end ) as charged_money
+		,abs(sum(case when Revenue<0 then Revenue else 0 end)) as refund_money  
+		,sum(Revenue) as vip_revenue
 	FROM
 		(
-		SELECT 
-		Product_ID 
-		,Charged_Amount
-		,ARRAY[Country_of_Buyer,'TOTAL'] as country_code
-		,Currency_of_Sale
-		,Financial_Status
-		,Order_Charged_Date
-		,Item_Price
-		,substring(cast(Order_Charged_Date as string),1,7) as stats_mon
-		FROM `hzdw2024.googleplay.p_sales_new_downloader`  
+		SELECT
+			Order_Charged_Date
+			,ARRAY[Country_of_Buyer,'TOTAL'] as country_code
+			,Revenue
+			,Product_ID package_name
+		FROM
+			gzdw2024.googleplay.dwd_gp_sales_event_di 
 		WHERE Order_Charged_Date >=date_add(run_date,interval -history_day day)
-		and Order_Charged_Date<=date_add(run_date,interval -end_day day)
-		)a 
-		left join 
-		(
-		SELECT 
-			stats_mon
-			,currency
-			,rate 
-		FROM `gzdw2024.gz_dim.exchange_rate` 
-		)b 
-		on a.stats_mon=b.stats_mon
-		and a.Currency_of_Sale=b.currency
-		join 
-		(
-			SELECT package_name 
-			FROM `gzdw2024.gz_dim.app_info` 
-			where platform='android'
-			group by package_name
-		)c 
-		on a.Product_ID=c.package_name
+			and Order_Charged_Date<=date_add(run_date,interval -end_day day)
+			
+			)a 
 		,UNNEST(country_code) as country_code
-		GROUP BY package_name,Order_Charged_Date,country_code;
+		group by stats_date,package_name,upper(country_code);
 
-
-
-
------------gp订阅收入,all1
-delete gzdw2024.revenue.dws_app_country_vip_income
-where stats_date>=date_add(run_date,interval -history_day day)
-and stats_date<=date_add(run_date,interval -end_day day)
-and package_name in ( SELECT package_name FROM `gzdw2024.gz_dim.app_info` where vip_income_table='hzdw2024.googleplay.p_sales_downloader');
-
-
-	insert    gzdw2024.revenue.dws_app_country_vip_income 
-	SELECT
-		Order_Charged_Date as stats_date
-		,package_name
-		,upper(country_code) as country_code
-	,sum(case when Financial_Status='Charged' then  Item_Price/rate else 0 end) as charged_money
-		,sum(case when Financial_Status='Refund' then  Item_Price/rate else 0 end) as refund_money  
-		,sum(case when Financial_Status='Charged' then  Item_Price/rate else 0 end)- sum(case when Financial_Status='Refund' then  Item_Price/rate else 0 end) as vip_revenue
-	FROM
-		(
-		SELECT 
-		Product_ID 
-		,Charged_Amount
-		,ARRAY[Country_of_Buyer,'TOTAL'] as country_code
-		,Currency_of_Sale
-		,Financial_Status
-		,Order_Charged_Date
-		,Item_Price
-		,substring(cast(Order_Charged_Date as string),1,7) as stats_mon
-		FROM `hzdw2024.googleplay.p_sales_downloader`  
-		WHERE Order_Charged_Date >=date_add(run_date,interval -history_day day)
-		and Order_Charged_Date<=date_add(run_date,interval -end_day day)
-		)a 
-		left join 
-		(
-		SELECT 
-			stats_mon
-			,currency
-			,rate 
-		FROM `gzdw2024.gz_dim.exchange_rate` 
-		)b 
-		on a.stats_mon=b.stats_mon
-		and a.Currency_of_Sale=b.currency
-		join 
-		(
-			SELECT package_name 
-			FROM `gzdw2024.gz_dim.app_info` 
-			where platform='android'
-			group by package_name
-		)c 
-		on a.Product_ID=c.package_name
-		,UNNEST(country_code) as country_code
-		GROUP BY package_name,Order_Charged_Date,country_code;
 
 -----applestore订阅收入atlasv
 delete gzdw2024.revenue.dws_app_country_vip_income
