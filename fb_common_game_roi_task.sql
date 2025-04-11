4506457d4ef59d47a483e5bfd855f457d4aa08fd
lucaliu958 <xuewang@atlasv.com>
2025-04-11 16:44:22 +0800

    Update fb_common_game_roi_task.sql
    增加quiz收入统计

--- fb_common_game_roi_task.sql
+++ fb_common_game_roi_task.sql
@@ -344,6 +344,7 @@ insert `gzdw2024.fbgame_03_bi.dws_fb_common_game_ad_revenue_daily_reports`
 						when app_name='Bubble Shoot Pro' then 'fb.bubble.shoot.pro'
 						when app_name='Save The Dog' then 'fb.save.dog' 
 						 when app_name='Egg Shoot Dino' then 'fb.egg.bubble'
+							when app_name='Quiz' then 'fb.quiz' 
 						else 'other' end as package_name
 					,'TOTAL' as platform
 					,'TOTAL' as country_code
@@ -371,6 +372,7 @@ insert `gzdw2024.fbgame_03_bi.dws_fb_common_game_ad_revenue_daily_reports`
 						when app_name='Bubble Shoot Pro' then 'fb.bubble.shoot.pro'
 						when app_name='Save The Dog' then 'fb.save.dog' 
 						 when app_name='Egg Shoot Dino' then 'fb.egg.bubble'
+							when app_name='Quiz' then 'fb.quiz' 
 						else 'other' end as package_name
 					,case when lower(platform)='ios' then 'iOS' when lower(platform)='android' then 'Android' else 'web' end  as platform
 					,upper(country) as country_code
@@ -397,6 +399,7 @@ insert `gzdw2024.fbgame_03_bi.dws_fb_common_game_ad_revenue_daily_reports`
 						when app_name='Bubble Shoot Pro' then 'fb.bubble.shoot.pro'
 						when app_name='Save The Dog' then 'fb.save.dog' 
 						 when app_name='Egg Shoot Dino' then 'fb.egg.bubble'
+							when app_name='Quiz' then 'fb.quiz' 
 						else 'other' end as package_name
 					,case when lower(platform)='ios' then 'iOS' when lower(platform)='android' then 'Android' else 'web' end  as platform
 					,'TOTAL' as country_code
@@ -424,6 +427,7 @@ insert `gzdw2024.fbgame_03_bi.dws_fb_common_game_ad_revenue_daily_reports`
 						when app_name='Bubble Shoot Pro' then 'fb.bubble.shoot.pro'
 						when app_name='Save The Dog' then 'fb.save.dog' 
 						 when app_name='Egg Shoot Dino' then 'fb.egg.bubble'
+							when app_name='Quiz' then 'fb.quiz' 
 						else 'other' end as package_name
 					,'TOTAL'  as platform
 					,upper(country) as country_code
