efc044bb2c960fb251f6c483b99bcde42013667f
lucaliu958 <xuewang@atlasv.com>
2025-04-11 16:05:34 +0800

    Update fb_common_game_roi_task.sql
    增加quiz统计

--- fb_common_game_roi_task.sql
+++ fb_common_game_roi_task.sql
@@ -450,6 +450,7 @@ insert `gzdw2024.fbgame_03_bi.dws_fb_common_game_ad_revenue_daily_reports`
 						when app_name='Bubble Shoot Pro' then 'fb.bubble.shoot.pro'
 						when app_name='Save The Dog' then 'fb.save.dog' 
 						 when app_name='Egg Shoot Dino' then 'fb.egg.bubble'
+						when app_name='Quiz' then 'fb.quiz' 
 						else 'other' end as package_name
 					,'TOTAL' as platform
 					,'TOTAL' as country_code
@@ -465,59 +466,6 @@ insert `gzdw2024.fbgame_03_bi.dws_fb_common_game_ad_revenue_daily_reports`
 					and _TABLE_SUFFIX!='20241103'
 				--and date(start_timestamp)=parse_date('%Y%m%d',_table_suffix)
 				group by stats_date,package_name
-				union all 
-					SELECT 
-					date(stats_date) as stats_date
-					,case when app_name='Solitaire' then 'fb.zp' 
-						when app_name='OHO' then 'fb.ai.avatar.puzzle' 
-						when app_name='Bubble Pop Fruit' then 'fb.fruit.bubble' 
-						when app_name='AHA' then 'fb.ai.aha' 
-						when app_name='Fate Quest' then 'fb.otme.fate.quest' 
-						when app_name='Block Juggle' then 'fb.block.juggle' 
-						when app_name='Bubble Shoot Pro' then 'fb.bubble.shoot.pro'
-						when app_name='Save The Dog' then 'fb.save.dog' 
-						 when app_name='Egg Shoot Dino' then 'fb.egg.bubble'
-						else 'other' end as package_name
-					,case when lower(platform)='ios' then 'iOS' when lower(platform)='android' then 'Android' else 'web' end  as platform
-					,upper(country) as country_code
-					,sum(requests)  as requests
-					,sum(filled_requests)  as filled_requests
-					,sum(impressions)  as impressions
-					,sum(revenue)  as revenue
-					,sum(clicks)  as clicks
-				FROM `fb-ai-avatar-puzzle.analytics_439907691.facebook_ad_backup_detail_day` 
-				where stats_date >= date_add(run_date,interval -history_day day)
-			    and stats_date <= date_add(run_date,interval -history_end_day day)	
-				--	and app_name='Solitaire'
-				--and date(start_timestamp)=parse_date('%Y%m%d',_table_suffix)
-				group by stats_date,platform,country_code,package_name
-				union all 
-				SELECT 
-					date(start_timestamp) as stats_date
-					,case when app_name='Solitaire' then 'fb.zp' 
-						when app_name='OHO' then 'fb.ai.avatar.puzzle' 
-						when app_name='Bubble Pop Fruit' then 'fb.fruit.bubble' 
-						when app_name='AHA' then 'fb.ai.aha' 
-						when app_name='Fate Quest' then 'fb.otme.fate.quest' 
-						when app_name='Block Juggle' then 'fb.block.juggle' 
-						when app_name='Bubble Shoot Pro' then 'fb.bubble.shoot.pro'
-						when app_name='Save The Dog' then 'fb.save.dog' 
-						 when app_name='Egg Shoot Dino' then 'fb.egg.bubble'
-						else 'other' end as package_name
-					,case when lower(platform)='ios' then 'iOS' when lower(platform)='android' then 'Android' else 'web' end  as platform
-					,'TOTAL' as country_code
-					,sum(requests)  as requests
-					,sum(filled_requests)  as filled_requests
-					,sum(impressions)  as impressions
-					,sum(revenue)  as revenue
-					,sum(clicks)  as clicks
-					FROM `fb-ai-avatar-puzzle.analytics_439907691.facebook_ad_backup_detail_day` 
-				where stats_date >= date_add(run_date,interval -history_day day)
-			    and stats_date <= date_add(run_date,interval -history_end_day day)		
-					--and app_name='Solitaire'
-					--and app_name='Solitaire'
-				--and date(start_timestamp)=parse_date('%Y%m%d',_table_suffix)
-				group by stats_date,platform,country_code,package_name
 					union all 
 					SELECT 
 					date(start_timestamp) as stats_date
@@ -530,6 +478,7 @@ insert `gzdw2024.fbgame_03_bi.dws_fb_common_game_ad_revenue_daily_reports`
 						when app_name='Bubble Shoot Pro' then 'fb.bubble.shoot.pro'
 						when app_name='Save The Dog' then 'fb.save.dog' 
 						 when app_name='Egg Shoot Dino' then 'fb.egg.bubble'
+						when app_name='Quiz' then 'fb.quiz' 
 						else 'other' end as package_name
 					,'TOTAL'  as platform
 					,upper(country) as country_code
